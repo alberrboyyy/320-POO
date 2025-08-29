987ee1ac357a41e8539a499b31e8bc2e9712b71c
alberr_b0yyy <albertbraimi@gmail.com>
2025-08-29 11:02:35 +0200

    ajout de l'escargot sans poo

---
+++ personnal/snail/snail/snail/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace snail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();  
+
+            int rand = rnd.Next(1, 100);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(100);
+
+                Console.SetCursorPosition(i, 7);
+                Console.Write(" __O.");
+                Console.SetCursorPosition(i, 7);
+            }
+            Console.Write(" ____");
+
+            while (true) ;
+        }
+
+    }
+}
